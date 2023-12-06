6ac441233bbdbd64f68e2cf826d428d4b8aa4742
Raja-Birla18 <raja.birla2002@gmail.com>
2023-12-06 12:12:41 +0530

    new component added

--- pages/context/Comp3.js
+++ pages/context/Comp3.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import Comp4 from "./comp4";
 function Comp3(){
     return(
         <>
-        <h1>My name is raja birla</h1>
+        {/* <h1>My name is raja birla</h1> */}
+        <Comp4 />
         
         </>
     )
---
+++ pages/context/comp4.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Comp4(){
+    return(
+        <>
+        <h1>My name is raja birla</h1>
+        
+        </>
+    )
+}
+export default Comp4;
